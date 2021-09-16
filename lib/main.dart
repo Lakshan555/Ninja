0af754ba92fe76c33cac7a48229c54011fdcaeee
Lakshan <onlineipl18@gmail.com>
2021-09-17 00:53:08 +0530

    add body(Add TEXT,SizedBox)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() =>runApp(MaterialApp(
@@ -12,12 +13,39 @@ class NinjaCard extends StatelessWidget {
 
     return Scaffold(
       backgroundColor:Colors.grey[900],
+
       appBar: AppBar(
         title: Text('Ninja ID Card'),
         centerTitle: true,
         backgroundColor: Colors.grey[850],
         elevation: 0.0,
 
+      ),
+
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0 , 30.0, 0) ,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "NAME",
+              style:TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2.0,
+              ) ,
+            ),
+            SizedBox(height: 10.0,),
+            Text(
+              "IShanka",
+                style:TextStyle(
+                color: Colors.amberAccent[200],
+                fontSize: 28.0,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold
+              ) ,
+            ),
+          ],
+        ),
       ),
     );
   }
